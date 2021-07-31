d7800d41bde29d96d7e811bed3227548f4791944
Fitsum A <fitsumcs@gmail.com>
2021-07-31 11:11:56 +0300

    Creating an Empty Graph

--- Lab_01/graphdemo.py
+++ Lab_01/graphdemo.py
@@ -3,3 +3,11 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
+#Create a Graph 
+
+G = nx.Graph()                #Undirected Simple Graph 
+G1 = nx.DiGraph()             #Directed Simple Graph 
+ 
+ 
+
+ 
